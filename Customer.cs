742885f3ec7cacb1351a0ebfccfb39a9efa4f778
a7mdt <125561779+a7mdt@users.noreply.github.com>
2023-12-08 21:02:13 +0300

    Add files via upload

---
+++ Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopPracticalExam
+{
+    class Customer : Person
+    {
+        public string AccountNumber { get; set; }
+
+        public Customer(string name, int age, string accountNumber) : base(name, age)
+        {
+            AccountNumber = accountNumber;
+        }
+
+        public override string ToString()
+        {
+            return $"Account Number: {AccountNumber}, {base.ToString()}";
+        }
+    }
+}
---
+++ Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopPracticalExam
+{
+    // This class representing bank employees
+    class Employee : Person
+    {
+        public string EmployeeId { get; set; }
+
+        public Employee(string name, int age, string employeeId) : base(name, age)
+        {
+            EmployeeId = employeeId;
+        }
+
+        public override string ToString()
+        {
+            return $"Employee ID: {EmployeeId}, {base.ToString()}";
+        }
+    }
+}
